7a710d6d1938e72c2eca179a77435c4de2108614
Alyssa Rae Anderson <aly97@byu.edu>
2019-12-02 18:09:01 -0700

    Adding Database Context

---
+++ Intex/DAL/NorthwestLabsContext.cs
@@ -0,0 +1,22 @@
+﻿using Intex.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Intex.DAL
+{
+    public class NorthwestLabsContext : DbContext
+    {
+        public NorthwestLabsContext()
+            :base("NorthwestContext")
+        {
+
+        }
+
+        public DbSet<User_R>
+
+
+    }
+}
